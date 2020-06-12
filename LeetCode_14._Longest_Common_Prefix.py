c5d6db82e8dcddb9790c6d565e14e18d85ed9015
Huang-Min-Xiong <qwe31914@gmail.com>
2020-06-12 23:06:21 +0800

    LeetCode_14._Longest_Common_Prefix

---
+++ LeetCode_14._Longest_Common_Prefix.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        if strs==[]:
+            return ""
+
+        s1=min(strs)
+        s2=max(strs)
+
+        for i,j in enumerate(s1):
+            if j not in s2[i]:
+                return s1[:i]
+        return s1    
+
+#Example
+s=Solution()
+s.longestCommonPrefix(["flower","flow","flight"])
